fd899a2405461b11e57735c3a5acccfbec7f60eb
omowonuola <oyedmola@gmail.com>
2025-02-11 22:52:07 +0100

    adds the function to validate account id

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -1,7 +1,46 @@
 import TicketTypeRequest from './lib/TicketTypeRequest.js';
 import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketPaymentService from '../thirdparty/paymentgateway/TicketPaymentService.js';
+import SeatReservationService from '../thirdparty/seatbooking/SeatReservationService.js';
 
 export default class TicketService {
 
+  #paymentService;
+  #reservationService;
+  #TICKET_PRICES = {
+    'INFANT': 0,
+    'CHILD': 15,
+    'ADULT': 25
+  };
+  #MAX_TICKETS = 25;
 
+  constructor(
+    paymentService = new TicketPaymentService(), 
+    reservationService = new SeatReservationService()
+) {
+    this.#paymentService = paymentService;
+    this.#reservationService = reservationService;
+  }
+
+      /**
+   * Purchase tickets for the given account
+   * @param {number} accountId - The account ID making the purchase
+   * @param {...TicketTypeRequest} ticketTypeRequests - The ticket requests
+   * @throws {InvalidPurchaseException} If the purchase request is invalid
+   */
+      purchaseTickets(accountId, ...ticketTypeRequests) {
+
+        this.#validateAccountId(accountId);
+      }
+
+
+         /**
+   * Validate the account ID
+   * @private
+   */
+    #validateAccountId(accountId) {
+      if (!Number.isInteger(accountId) || accountId <= 0) {
+        throw new InvalidPurchaseException('Invalid account ID');
+      }
+  }
 }
