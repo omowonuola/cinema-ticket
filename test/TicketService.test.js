f6aa3245df3cc616be2cb92d22754675a7e65468
omowonuola <oyedmola@gmail.com>
2025-02-12 01:24:39 +0100

    adds the test suite for the successful purchase of adult tickets function

--- test/TicketService.test.js
+++ test/TicketService.test.js
@@ -16,3 +16,38 @@ class MockReservationService {
       this.lastReservation = { accountId, totalSeats };
     }
 }
+
+
+
+describe('TicketService', () => {
+    let ticketService;
+    let mockPaymentService;
+    let mockReservationService;
+  
+    beforeEach(() => {
+      mockPaymentService = new MockPaymentService();
+      mockReservationService = new MockReservationService();
+      ticketService = new TicketService(mockPaymentService, mockReservationService);
+    });
+
+
+    describe('purchaseTickets', () => {
+
+        it('should successfully purchase adult tickets', () => {
+          const request = new TicketTypeRequest('ADULT', 2);
+          ticketService.purchaseTickets(1, request);
+    
+          expect(mockPaymentService.lastPayment).to.deep.equal({
+            accountId: 1,
+            totalAmount: 50 // 2 adults * £25
+          });
+          expect(mockReservationService.lastReservation).to.deep.equal({
+            accountId: 1,
+            totalSeats: 2
+          });
+        });
+
+
+    });
+
+});
